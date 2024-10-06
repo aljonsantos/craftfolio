af59696a29e8acf87cb4dda5e97b3928185a0225
aljonsantos <cs.aljonssantos@gmail.com>
2024-10-06 18:37:46 +0800

    add familiar section in technical skills

--- src/components/Preview/Skills.jsx
+++ src/components/Preview/Skills.jsx
@@ -4,12 +4,13 @@ const skills = {
   programmingLanguages: ['JavaScript', 'Typescript', 'Python', 'Java', 'HTML', 'CSS'],
   libraryFrameworks: ['React', 'Redux', 'Node.js', 'Express', 'GraphQL', 'Next.js', 'TailwindCSS'],
   toolsPlatforms: ['Git', 'GitHub', 'Vercel', 'Firebase', 'PostgreSQL', 'MySQL', 'MongoDB', 'Docker', 'AWS S3'],
+  familiar: ['C', 'C++', 'C#', 'PHP', 'SASS', 'Bootstrap', 'Material-UI', 'Chakra-UI', 'Jest', 'React Testing Library']
 }
 
 const Part = ({ title, content }) => {
   return (
     <div className="flex flex-col gap-3">
-      <p className="text-content-800 font-semibold pl-1">{title}</p>
+      {title && <p className="text-content-700 font-semibold pl-1">{title}</p>}
       <div className="flex flex-wrap gap-2 mb-2 md:text-sm">
         {content.map((item, index) => (
           <div key={index} className=" text-accent border border-accent-100/70 bg-accent/10 px-2 rounded-3xl">{item}</div>
@@ -23,9 +24,12 @@ const Skills = () => {
   return (
     <Section title="Skills">
       <div className="flex flex-col gap-3">
+        <p className="font-bold text-accent-700 ml-1">Proficient</p>
         <Part title="Programming Languages" content={skills.programmingLanguages} />
         <Part title="Library & Frameworks" content={skills.libraryFrameworks} />
         <Part title="Tools & Platforms" content={skills.toolsPlatforms} />
+        <p className="font-bold text-accent-700 ml-1">Familiar</p>
+        <Part content={skills.familiar} />
       </div>
     </Section>
   )
